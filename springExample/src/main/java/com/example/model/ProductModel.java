4e74bd49f5ee3fc1f3dba1fefee2c2c0be71fdde
Rakib <rakib.hasan.asia@gmail.com>
2022-01-01 01:30:27 +0600

    first commit

---
+++ springExample/src/main/java/com/example/model/ProductModel.java
@@ -0,0 +1,44 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+
+@Entity
+@Table(name = "products")
+public class ProductModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private int price;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+
+}
